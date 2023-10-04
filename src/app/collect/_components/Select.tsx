08273b6314daa07a78e8361d975c49035aa989b0
yesoryeseul <yesoryeseul07@gmail.com>
2023-10-05 02:15:58 +0900

    feat: select 기능 추가

--- src/app/collect/_components/Select.tsx
+++ src/app/collect/_components/Select.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { useState } from 'react';
+
 import {
   Select,
   SelectContent,
@@ -5,17 +8,28 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+interface FilterSelectProps {
+  onValueChange: (value: string) => void;
+}
 
-const FilterSelect = () => {
+const FilterSelect: React.FC<FilterSelectProps> = ({ onValueChange }) => {
+  const [selectedValue, setSelectedValue] = useState<string>('');
+  const handleSelectChange = (value: string) => {
+    setSelectedValue(value); // 선택한 값을 업데이트
+    if (onValueChange) {
+      onValueChange(value);
+    }
+  };
   return (
-    <Select>
+    <Select value={selectedValue} onValueChange={handleSelectChange}>
       <SelectTrigger className='w-[180px]'>
         <SelectValue placeholder='전체' />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value='light'>Light</SelectItem>
-        <SelectItem value='dark'>Dark</SelectItem>
-        <SelectItem value='system'>System</SelectItem>
+        <SelectItem value='전체'>전체</SelectItem>
+        <SelectItem value='콘서트'>콘서트</SelectItem>
+        <SelectItem value='클래식'>클래식</SelectItem>
+        <SelectItem value='기타'>기타</SelectItem>
       </SelectContent>
     </Select>
   );
--- src/app/collect/page.tsx
+++ src/app/collect/page.tsx
@@ -11,6 +11,7 @@ export interface CulturalEventRow {
   MAIN_IMG: string;
   ORG_NAME: string;
   TITLE: string;
+  CODENAME: string;
 }
 
 interface CulturalEventInfo {
@@ -23,6 +24,7 @@ interface CulturalEventInfo {
 const Collect = () => {
   const apiKey = process.env.NEXT_PUBLIC_API_KEY as string;
   const [data, setData] = useState<CulturalEventInfo | null>(null);
+  const [selectedValue, setSelectedValue] = useState<string>('');
 
   useEffect(() => {
     CollectAPI(1, 50)
@@ -33,19 +35,24 @@ const Collect = () => {
       .catch((error) => {
         console.error('에러 발생', error);
       });
-  }, [apiKey]);
+  }, [apiKey, selectedValue]);
 
   return (
     <div className='flex flex-col items-center max-w-7xl m-auto'>
       <h1 className='text-4xl font-bold my-16'>행사 모아보기</h1>
-      <FilterSelect />
+      <FilterSelect onValueChange={setSelectedValue} />
       {data ? (
         <div className='grid grid-cols-3 gap-4'>
-          {data?.culturalEventInfo?.row?.map((item, idx) => (
-            <div key={idx} className='p-4'>
-              <OneItem item={item} />
-            </div>
-          ))}
+          {data?.culturalEventInfo?.row
+            ?.filter(
+              (item) =>
+                !selectedValue || selectedValue === '전체' || item.CODENAME === selectedValue,
+            ) // 선택한 값이 없거나 값이 전체이거나 CODENAME과 일치하는 경우에만 필터링
+            .map((item, idx) => (
+              <div key={idx} className='p-4'>
+                <OneItem item={item} />
+              </div>
+            ))}
         </div>
       ) : (
         <SkeletonLoader />
