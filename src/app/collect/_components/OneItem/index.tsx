d0b8f354f3ed476f2ace814ee513b75ddcb39f27
yesoryeseul <yesoryeseul07@gmail.com>
2023-10-05 12:17:30 +0900

    remove: 주석 제거

--- src/app/collect/_components/OneItem/index.tsx
+++ src/app/collect/_components/OneItem/index.tsx
@@ -1,21 +1,3 @@
-// import Image from 'next/image';
-
-// import { CulturalEventRow } from '../../page';
-
-// const OneItem = ({ item }: { item: CulturalEventRow }) => {
-//   const { MAIN_IMG, ORG_NAME, TITLE } = item;
-//   return (
-//     <div>
-//       <Image src={MAIN_IMG} width={400} height={562} alt='test' />
-//       <div className='flex flex-col items-center my-4'>
-//         <p className='text-lg text-gray-500'>{ORG_NAME}</p>
-//         <p className='text-xl font-bold text-gray-900 mt-2'>{TITLE}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default OneItem;
 'use client';
 import Image from 'next/image';
 
