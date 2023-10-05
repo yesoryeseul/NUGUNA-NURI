a0adc454d47d84c48540a4f950546f26f49ec4d2
yesoryeseul <yesoryeseul07@gmail.com>
2023-10-05 23:54:59 +0900

    refactor: 상세페이지 연결 Link 태그로 수정

--- src/app/collect/_components/OneItem/index.tsx
+++ src/app/collect/_components/OneItem/index.tsx
@@ -1,18 +1,13 @@
-'use client';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 import { CulturalEventRow } from '../../page';
 
 const OneItem = ({ item, idx }: { item: CulturalEventRow; idx: number }) => {
   const { MAIN_IMG, ORG_NAME, TITLE } = item;
-  const router = useRouter();
-  const handleItemClick = () => {
-    console.log(idx);
-    router.push(`/collect/${idx}`);
-  };
+  const url = `/collect/${idx}`;
   return (
-    <div onClick={handleItemClick} style={{ cursor: 'pointer' }}>
+    <Link href={url}>
       <Image
         src={MAIN_IMG}
         width={400}
@@ -25,7 +20,7 @@ const OneItem = ({ item, idx }: { item: CulturalEventRow; idx: number }) => {
         <p className='text-lg text-gray-500'>{ORG_NAME}</p>
         <p className='text-xl font-bold text-gray-900 mt-2'>{TITLE}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
