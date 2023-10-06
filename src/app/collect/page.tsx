f4854283232ffbab33246488a6f4d3370848cc32
yesoryeseul <yesoryeseul07@gmail.com>
2023-10-06 23:21:09 +0900

    feat: 행사 모아보기 페이지네이션 구현

--- src/app/collect/page.tsx
+++ src/app/collect/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { atom, useAtom } from 'jotai';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import CollectAPI from '@/api/collectAPI';
+import Pagination from '@/components/Pagination/Pagination';
 import { selectValueAtom } from '@/types/collect/atom';
 
 import OneItem from './_components/OneItem';
@@ -24,22 +25,44 @@ interface CulturalEventInfo {
 }
 
 const dataAtom = atom<CulturalEventInfo | null>(null);
-
 const Collect = () => {
   const apiKey = process.env.NEXT_PUBLIC_API_KEY as string;
   const [data, setData] = useAtom(dataAtom);
   const [selectedValue, setSelectedValue] = useAtom(selectValueAtom);
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태 추가
+  const [totalPage, setTotalPage] = useState(0);
+  const itemsPerPage = 12; // 한 페이지에 표시할 아이템 수
 
   useEffect(() => {
-    CollectAPI(1, 300)
+    let startIndex: number;
+    let endIndex: number;
+
+    if (currentPage > 1) {
+      startIndex = (currentPage - 1) * itemsPerPage;
+      endIndex = startIndex + itemsPerPage - 1;
+    } else {
+      startIndex = 0; // 시작 페이지에서는 0으로 시작
+      endIndex = itemsPerPage - 1;
+    }
+
+    // CollectAPI 함수 호출 시 데이터 범위를 인자로 전달
+    CollectAPI(0, 300, selectedValue) // selectedValue를 추가하여 필터링 조건을 전달
       .then((dataString) => {
         const parsedData = JSON.parse(dataString);
-        setData(parsedData);
+        const slicedData = parsedData.culturalEventInfo.row.slice(startIndex, endIndex + 1);
+        setData({ ...parsedData, culturalEventInfo: { row: slicedData } });
+        console.log('test', parsedData);
+        setTotalPage(parsedData?.culturalEventInfo?.row?.length);
       })
       .catch((error) => {
         console.error('에러 발생', error);
       });
-  }, [apiKey, selectedValue, setData]);
+  }, [apiKey, selectedValue, setData, currentPage, itemsPerPage]);
+
+  // Pagination 페이지 변경 시 해당 페이지에 맞는 데이터 불러오기
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <div className='flex flex-col items-center max-w-7xl m-auto'>
@@ -47,20 +70,23 @@ const Collect = () => {
       <FilterSelect onValueChange={setSelectedValue} />
       {data ? (
         <div className='grid grid-cols-3 gap-4'>
-          {data?.culturalEventInfo?.row
-            ?.filter(
-              (item) =>
-                !selectedValue || selectedValue === '전체' || item.CODENAME === selectedValue,
-            ) // 선택한 값이 없거나 값이 전체이거나 CODENAME과 일치하는 경우에만 필터링
-            .map((item, idx) => (
-              <div key={idx} className='p-4'>
-                <OneItem item={item} idx={idx} />
-              </div>
-            ))}
+          {data?.culturalEventInfo?.row?.map((item, idx) => (
+            <div key={idx} className='p-4'>
+              <OneItem item={item} idx={idx} />
+            </div>
+          ))}
         </div>
       ) : (
         <SkeletonLoader />
       )}
+      <div className='my-12'>
+        <Pagination
+          total_count={totalPage}
+          list={itemsPerPage}
+          itemPerPage={10}
+          onPageChange={handlePageChange}
+        />
+      </div>
     </div>
   );
 };
