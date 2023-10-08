e5130e9968469682850ae4575a9b2950fcb4a964
heesuePark <phs8743@naver.com>
2023-10-09 00:27:15 +0900

    feat:리액트훅폼으로 유효성검사 완료

--- src/app/review/_components/CommentForm/index.tsx
+++ src/app/review/_components/CommentForm/index.tsx
@@ -1,11 +1,30 @@
+'use client';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
+
+interface CommentTextArea {
+  comment: string;
+}
 const CommentForm = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<CommentTextArea>();
+  const onSubmit: SubmitHandler<CommentTextArea> = (data) => console.log(data);
   return (
-    <div className='grid w-full gap-2 my-7'>
-      <Textarea placeholder='댓글을 입력해주세요' />
+    <form onSubmit={handleSubmit(onSubmit)} className='grid w-full gap-2 my-7'>
+      <Textarea
+        {...register('comment', { minLength: 5, maxLength: 250 })}
+        placeholder='댓글을 입력해주세요'
+      />
+      {errors.comment && (
+        <p className='text-red-400 text-sm'>5자 이상, 250자 이내로 작성해주세요.</p>
+      )}
       <Button>등록하기</Button>
-    </div>
+    </form>
   );
 };
 export default CommentForm;
