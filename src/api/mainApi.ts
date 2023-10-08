e46a9a4a3089aed5d7c96c4c0930fb2e5929c90e
heesuePark <phs8743@naver.com>
2023-10-08 15:10:25 +0900

    chore:오류 고치는 중

---
+++ src/api/mainApi.ts
@@ -0,0 +1,20 @@
+import xml2js from 'xml2js';
+
+import { ApiRes, ApiType } from '../types/main/types';
+const mainApi = async (startIndex: number, endIndex: number): Promise<ApiType[]> => {
+  const apiKey = process.env.MAIN_API_KEY;
+  const url = `http://openapi.seoul.go.kr:8088/${apiKey}/xml/culturalEventInfo/${startIndex}/${endIndex}/`;
+  const res = await fetch(url, { cache: 'no-store' });
+  const data = await res.text();
+
+  const parser = new xml2js.Parser();
+  const apis: ApiRes = await new Promise((resolve, reject): void => {
+    parser.parseString(data, (err, result) => {
+      if (err) reject(err);
+      else resolve(result);
+    });
+  });
+
+  return apis.culturalEventInfo.row;
+};
+export default mainApi;
